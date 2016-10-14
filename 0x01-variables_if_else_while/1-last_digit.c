5ebb9f8a2e30e5d11ef458ab8bf2db17c6ce0830
carlawash <carlawashington415@gmail.com>
2016-10-14 21:15:30 +0000

    greaterthan/lessthan

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * less than/greater than
+ * Return: (0)
+ */
+int main(void)
+{
+	int n, d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10;
+	printf("Last digit of %d is %d ", n, d);
+	if (d > 5)
+		printf("and is greater than 5\n");
+	else if (d != 0)
+		printf("and is less than 6 and not 0\n");
+	else
+		printf("and is 0\n");
+	return (0);
+}
