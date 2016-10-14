594ffcaf3b7bada24378a8fd74bf7c721d85d744
carlawash <carlawashington415@gmail.com>
2016-10-14 23:35:02 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * numbers
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
