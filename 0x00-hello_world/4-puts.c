fff6a0ab4ae91979e6a5fea04b76c9d0d9422ef3
carlawash <carlawashington415@gmail.com>
2016-10-14 00:22:49 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - puts
+ *
+ * Description: prints phrase
+ *
+ * Return: 0
+ */
 int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle\n);
+	puts("Programming is like building a multilingual puzzle\n);
 }
