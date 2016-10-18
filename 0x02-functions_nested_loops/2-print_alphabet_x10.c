39ae92d74a860da8470f0df89f6704d5f4868a49
carlawash <carlawashington415@gmail.com>
2016-10-18 19:51:51 +0000

    printalphaX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - print_alphabet
+ *
+ * Return: (0)
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i = 0
+	while (i<10)
+	{
+	a = 'a';
+	while (a <= 'z')
+		_putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+}
