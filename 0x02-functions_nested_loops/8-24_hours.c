9c43b7aa8949a2d0571e63dd53c89b9c25e73f45
carlawash <carlawashington415@gmail.com>
2016-10-20 01:13:00 +0000

    nestedloop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ * Return:(0)
+ */
+
+void jack_bauer(void)
+{
+	int hour_a;
+	int hour_b;
+	int min_a;
+	int min_b;
+	int hour;
+	int min;
+
+	hour = 0;
+	while (hour < 24)
+	{
+		hour_a = hour / 10;
+		hour_b = hour % 10;
+
+		min = 0;
+		while (min < 60)
+		{
+			min_a = min / 10;
+			min_b = min % 10;
+			_putchar(hour_a + '0');
+			_putchar(hour_b + '0');
+			_putchar(':');
+			_putchar(min_a + '0');
+			_putchar(min_b + '0');
+			_putchar('\n');
+			min++;
+		}
+		hour++;
+
+	}
+
+
+}
