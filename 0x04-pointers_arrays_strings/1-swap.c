90453c0319571e4f825ba74250f2bfab3d3c4b30
carlawash <carlawashington415@gmail.com>
2016-10-25 01:01:16 +0000

    swap values

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ *
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	*a = 42;
+	*b = 98;
+}
