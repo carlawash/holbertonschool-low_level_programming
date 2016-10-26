ea12521022a8c2b9cdb9930be8506fa31be73744
carlawash <carlawashington415@gmail.com>
2016-10-26 22:02:24 +0000

    print reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - reverse
+ * @s: prints string in reverse
+ *
+ */
+
+void print_rev(char *s)
+
+{
+	int e;
+
+	e = 0;
+	while (s[e] != '\0')
+	{
+		e++;
+	}
+	while (e >= 0)
+	{
+		_putchar(s[e]);
+		e--;
+	}
+
+	_putchar('\n');
+}
