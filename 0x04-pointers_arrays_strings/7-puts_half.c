38ddda7e173d81cbd45bdec9349782021ff7e9dc
carlawash <carlawashington415@gmail.com>
2016-11-02 01:32:47 +0000

    puts half

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,25 @@ void puts_half(char *str)
 
 {
 	int c;
+	int i;
 
-	c = 5;
+	c = 0;
 	while (str[c] != '\0')
 	{
-		_putchar(str[c]);
 		c++;
+	}
+	if (c % 2 == 0)
+	{
+		i = c / 2;
+	}
+	else
+	{
+		i = (c - 1) / 2;
+	}
+	while (i < c)
+	{
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
