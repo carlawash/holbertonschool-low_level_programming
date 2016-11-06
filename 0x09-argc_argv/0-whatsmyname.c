60c9a0ce1fe5c82d8b7e64f357e2ed2cf1955749
Carla <carlawashington415@gmail.com>
2016-11-05 22:58:38 -0700

    Update 0-whatsmyname.c

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
- */
+* main - print out executable
+* @argc: counter
+* @argv: value
+* Return: (0)
+*/
 int main(int agrc, char *agrv[])
 {
 	if (agrc <= 1)
-		printf 
+		printf("%s\n", argv[0]);
+	return (0);
+}
