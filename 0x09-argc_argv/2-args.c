60f2fd79e1049cb3d70b3caf2952a7b41ffb7d41
carlawash <carlawashington415@gmail.com>
2016-11-08 02:53:24 +0000

    args

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,1 +1,14 @@
-program
+#include <stdio.h>
+/**
+ * main - prints all arguments received
+ * @argc: counter
+ * @argv: value
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
+
