5342a8f3084ce6a3fcabf5ecd9bb49322b1281b2
carlawash <carlawashington415@gmail.com>
2016-11-10 03:56:20 +0000

    adding

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+/**
+* alloc_grid - dynamically allocate mem for 2D array
+* @width: width of array
+* @height: height of array
+* Return: starting address, otherwise NULL
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			i--;
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	return (grid);
+}
