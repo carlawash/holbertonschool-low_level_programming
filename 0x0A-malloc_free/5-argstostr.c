06c3e31312503a824f56383dd13438a1663bd2f1
carlawash <carlawashington415@gmail.com>
2016-11-10 07:50:14 +0000

    add

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concats args of program sep by new line
+ * @ac: number of inputs
+ * @av: input
+ * Return: address, or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *res, *temp;
+	int i, j, len;
+
+	len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+			j++;
+			len++;
+		}
+
+		len++;
+	}
+	len++;
+	res = malloc(len * sizeof(char));
+
+	if (res == NULL)
+		return (NULL);
+
+	temp = res;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+
+		{
+			*res = av[i][j];
+			j++;
+			res++;
+
+		}
+		*res = '\n';
+		res++;
+	}
+	return (temp);
+
+}
