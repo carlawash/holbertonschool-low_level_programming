d22f8d00e6e221054d0cab03b3b2f271e8ee7444
carlawash <carlawashington415@gmail.com>
2016-11-11 01:13:38 +0000

    add

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -5,8 +5,7 @@
  * main - add up positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: 1 if error, else 0
+ * Return: (0)
  */
 int main(int argc, char *argv[])
 {
